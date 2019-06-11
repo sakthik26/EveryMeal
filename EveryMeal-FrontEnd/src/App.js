1513c27463185d75b8ee5291be37fee1322dd693
Sakthi Kumar <sakthi@dhcp-10-181-43-199.dynamic.eduroam.mwn.de>
2019-06-11 11:33:34 +0200

    New

---
+++ EveryMeal-FrontEnd/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import logo from './Images/everyMealLogo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p> Every Meal </p>
+        <p>
+          Currently in development ...
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
