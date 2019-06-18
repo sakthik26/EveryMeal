bc7d9ce366c1a65336208f133ceb7b6b223cb6ce
smnwltr <smn@wltr.co>
2019-06-18 16:01:32 +0200

    #18 Add component scaffold and create basic header and footers

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,3 +1,51 @@
 "use strict";
 
 import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
+import logo from '../assets/images/everyMealLogo.png';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+
+const styles = theme => ({
+    appbar: {
+        backgroundColor: '#FFFF',
+        color: 'rgba(0, 0, 0, 0.87)'
+    },
+    logo: {
+        width: '75px',
+        height: '50px',
+    },
+    tabs: {
+        display: 'flex',
+        flexGrow: '1',
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
+})
+
+class Header extends React.Component {
+    render() {
+        const { classes } = this.props;
+
+        return (
+            <AppBar className={classes.appbar} position="static">
+                <Toolbar>
+                    <img className={classes.logo} src={logo} alt="logo" />
+                    <Tabs className={classes.tabs}>
+                        <Tab label="Eat Now" />
+                        <Tab label="Subscribe" />
+                    </Tabs>
+                    <Button>Sign Up</Button>
+                    <Button>Login</Button>
+                </Toolbar>
+            </AppBar>
+
+        )
+    }
+}
+
+export default withStyles(styles)(Header);
--- client/src/views/Dashboard.js
+++ client/src/views/Dashboard.js
@@ -1,14 +1,45 @@
 "use strict";
 
 import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import { withStyles } from '@material-ui/core/styles';
 
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
+const styles = theme => ({
+    main: {
+        padding: 24,
+    },
+})
+
 export class Dashboard extends React.Component {
     render() {
-        return 0;
+        const { classes } = this.props;
+        return (
+            <div>
+                <Header />
+
+                <Grid className={classes.main} container spacing={2}>
+                    <Grid item xs={12}>
+                        <h1>Food Consumption</h1>
+                    </Grid>
+
+                    <Grid item xs={12} md={6}>
+                        <h1>Macronutrients</h1>
+                    </Grid>
+
+                    <Grid item md={6}>
+                        <h1>Micronutrients</h1>
+                    </Grid>
+
+                </Grid>
+
+                <Footer />
+            </div>
+        );
+
     }
 }
 
-export default Dashboard;
+export default withStyles(styles)(Dashboard);
