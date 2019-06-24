3ac9382854797ad8234543c32388efb34df6c164
smnwltr <smn@wltr.co>
2019-06-24 16:57:54 +0200

    Merge

--- client/src/index.js
+++ client/src/index.js
@@ -4,17 +4,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { Provider } from 'react-redux';
-import configureStore from './Store/configureStore';
+// import configureStore from './Store/configureStore';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {Route, Link, BrowserRouter as Router} from "react-router-dom";
 import Signup from './views/Signup';
 import Dashboard from './views/Dashboard';
 
-const store = configureStore();
+// const store = configureStore();
 
 const routing = (
-  <Provider store={store}>
+  // <Provider store={store}>
     <Router>
       <div>
         <Route exact path="/" component= {App} />
@@ -22,7 +22,7 @@ const routing = (
         <Route path="/dashboard" component= {Dashboard} />
       </div>
     </Router>
-  </Provider>
+  // </Provider>
 )
 
 ReactDOM.render(routing, document.getElementById('root'));
