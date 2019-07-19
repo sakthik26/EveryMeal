b53f7d796fc0bfcae86daab3b28a49da9af3ac85
smnwltr <smn@wltr.co>
2019-07-19 17:55:05 -0600

    Unify usage of env variables and adjust naming for clarity Remove unnecessary db.js file and include code in regular app.js file Remove commented out code fragments

--- api/src/app.js
+++ api/src/app.js
@@ -1,52 +1,17 @@
-// const config = require("config");
-// const mongoose = require("mongoose");
-// const express = require("express");
-// const usersRoute = require("./routes/UserRoutes");
-// var cors = require('cors')
-
-// const app = express();
-// app.use(cors());
-
-// //use config module to get the privatekey, if no private key set, end the application
-// if (!config.get("myprivatekey")) {
-//   console.error("FATAL ERROR: myprivatekey is not defined.");
-//   process.exit(1);
-// }
-
-// //connect to mongodb
-// mongoose
-//   .connect("mongodb://localhost/everymeal_auth", { useNewUrlParser: true })
-//   .then(() => console.log("Connected to MongoDB..."))
-//   .catch(err => console.error("Could not connect to MongoDB..."));
-
-
-// app.use(express.json());
-// //use users route for api/users
-// app.get('/', function(req,res){
-//   res.send('Backend');
-// })
-// app.use('/api/users', usersRoute);
-// app.use('/api/users', usersRoute);
-
-
-// const port = process.env.PORT || 4000;
-
-// app.listen(port, function () {
-//   console.log(`Listening on port ${port}...`);
-// });
 
 const express = require('express');
-const port = process.env.PORT;
 const usersRoute = require("./routes/UserRoutes");
 const mealPlanRoute = require("./routes/MealPlanRoutes");
-//require('./db/db')
 const mongoose = require("mongoose");
-const cors = require('cors')
+const cors = require('cors');
+
+const port = process.env.EM_PORT;
+const mongoURI = process.env.EM_MONGOURI;
 
 const app = express();
 app.use(cors())
 mongoose
-  .connect("mongodb://localhost/everymeal_user_authentication", { useNewUrlParser: true,useCreateIndex: true })
+  .connect(mongoURI, { useNewUrlParser: true,useCreateIndex: true })
   .then(() => console.log("Connected to MongoDB..."))
   .catch(err => console.error("Could not connect to MongoDB..."));
 
@@ -58,8 +23,3 @@ app.use(mealPlanRoute);
 app.listen(port, () => {
   console.log(`Server running on port ${port}`)
 })
-
-
-// MONGODB_URL=mongodb://localhost/everymeal_user_authentication
-// JWT_KEY=WinterIsComing
-// PORT=4000
--- api/src/middleware/auth.js
+++ api/src/middleware/auth.js
@@ -1,28 +1,12 @@
-// const jwt = require('jsonwebtoken');
-// const config =  require('config');
-
-// module.exports = function(req,res,next){
-
-//     const token = req.headers["x-access-token"] || req.headers["authorization"];
-
-//     if (!token) return res.status(401).send("Access denied. No token provided.");
-
-//         try{
-//             const decoded = jwt.verify(token, config.get('myprivatekey'));
-//             req.user = decoded;
-//             next();
-//         } catch(e){
-//             res.status(400).send('Invalid token');
-//         }
-//  }
-
 const jwt = require('jsonwebtoken');
 const User = require('../models/UserModel')
+
+const jwtKey = process.env.EM_JWTSECRET;
 
 const auth = async function(req,res,next){
    const token = req.header('Authorization') !== undefined ? req.header('Authorization').replace('Bearer ','') : '';
    if(!token) return res.status(401).send({error:'Not authorized to access this resource'})
-   const data = jwt.verify(token,process.env.JWT_KEY);
+   const data = jwt.verify(token,jwtKey);
    try{
        const user = await User.findOne({_id: data._id,'tokens.token':token})
        if(!user){
